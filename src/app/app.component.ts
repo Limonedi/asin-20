ebb0af90ac3dda25357b4a3ff667ea3ab277dad8
Limonedi <holuloldoce@gmail.com>
2023-01-17 21:25:25 -0500

    actu final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './data.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'asin20';
+  posts: any;
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit() {
+    this.dataService.getData().subscribe(data => {
+      this.posts = data;
+    });
+}
 }
